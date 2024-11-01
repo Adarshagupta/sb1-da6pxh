8b64c5eb93ef2501a7fbd7d448a55fb6f10755b0
Adarsh Gupta <blueadarsh1@gmail.com>
2024-11-01 10:31:01 +0530

    fix error part 2

--- src/utils/sitemap-generator.ts
+++ src/utils/sitemap-generator.ts
@@ -1,9 +1,10 @@
 import { SitemapStream, streamToPromise } from 'sitemap';
+import type { SitemapItem } from 'sitemap';
 import { Readable } from 'stream';
 
 const generateSitemap = async (hostname: string) => {
   // An array of all pages in your site
-  const links = [
+  const links: SitemapItem[] = [
     { url: '/', changefreq: 'daily', priority: 1.0 },
     { url: '/generator', changefreq: 'daily', priority: 0.9 },
     { url: '/profile', changefreq: 'weekly', priority: 0.8 },
