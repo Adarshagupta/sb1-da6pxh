b3ea13c791c877b9c2c0665d0a5e575b233a39e9
Adarsh Gupta <blueadarsh1@gmail.com>
2024-11-04 01:11:10 +0530

    ok

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b004d73f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9lkh008trr"
+    "revision": "0.cib1et61idg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
